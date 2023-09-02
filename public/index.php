6baadfb9d315781e8479230195194d9a5a958348
banafsheh <banafsheh.azizi91@gmail.com>
2023-09-02 20:29:23 +0330

    add routes

--- public/index.php
+++ public/index.php
@@ -6,3 +6,26 @@ require_once '../config/app.php';
 require_once '../config/database.php';
 require_once '../vendor/autoload.php';
 require_once '../routes/web.php';
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        throw new HttpResponseException('Route Not Found', 404);
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        throw new \http\Exception\BadMethodCallException('Method Not Allowed', 405);
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $handler = explode('@', $handler);
+        $controller_name = $handler[0];
+        $method_name = $handler[1];
+        $controller = new $controller_name();
+        $_SERVER = array_merge($_SERVER, $routeInfo[2]);
+        return $controller->$method_name();
+        break;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,9 @@
 <?php
 
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $route) {
+    $route->addRoute('GET', '/shopping-list', 'App\Controllers\ShoppingController@index');
+    $route->addRoute('GET', '/shopping-items', 'App\Controllers\ShoppingController@list');
+    $route->addRoute('POST', '/shopping-items', 'App\Controllers\ShoppingController@insert');
+    $route->addRoute('PUT', '/shopping-items/{item_id}', 'App\Controllers\ShoppingController@update');
+    $route->addRoute('DELETE', '/shopping-items/{item_id}', 'App\Controllers\ShoppingController@delete');
+});
