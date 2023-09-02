a4b0bbae3d63369f5e8b97fcb044b712a37902e8
banafsheh <banafsheh.azizi91@gmail.com>
2023-09-02 19:56:51 +0330

    add models

---
+++ app/Models/ShoppingItems.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+class ShoppingItems extends Model
+{
+    private $id;
+    private $name;
+    private $status_id;
+    protected $table = "shopping_items";
+
+
+
+
+}
---
+++ app/Models/ShoppingItemsStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Models;
+
+
+class ShoppingItemsStatus extends Model
+{
+    private $id;
+    private $name;
+    protected $table = "shopping_items_status";
+
+}
